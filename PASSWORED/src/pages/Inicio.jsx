ae78641f8071812bfd2aa70fd213c0df87b1e755
franco sassi <franco.sassi97@gmail.com>
2024-09-08 15:33:15 -0300

    seccion1 mobile

--- PASSWORED/src/pages/Inicio.jsx
+++ PASSWORED/src/pages/Inicio.jsx
@@ -5,7 +5,8 @@ import Button from "../components/Button";
 import fondoInicio from "../assets/fondoInicio.png";
 import fondoInicioColor from "../assets/fondoInicioColor.png";
 import Estrella3 from "../assets/images/Estrella3";
-
+import fondoInicioPhone from "../assets/backImagePhone.png";
+import fondoInicioColorPhone from "../assets/backPhone.png";
 import styles from "./inicio.module.css";
  const Inicio = () => {
   return (
@@ -25,6 +26,10 @@ import styles from "./inicio.module.css";
         <div className={styles.desc}>
           <p className={styles.text1}>
             App que genera<br/>hábitos de lectura,<br/> comprensión de textos<br/> y mucho
+            más.
+          </p>
+          <p className={styles.text1Mobile}>
+            App que<br/> genera hábitos<br/> de lectura,<br/> comprensión<br/> de textos<br/> y mucho
             más.
           </p>
         </div>
@@ -50,6 +55,19 @@ import styles from "./inicio.module.css";
           src={fondoInicioColor}
           alt="fondo inicio"
           className={styles.backImage}
+        />
+      </div>
+      <div className={styles.section21}>
+        <img
+          src={fondoInicioPhone}
+          alt="fondo inicio"
+          //  className={styles.overlayImage1}
+          className={styles.backgroundImagePhone}
+        />
+        <img
+          src={fondoInicioColorPhone}
+          alt="fondo inicio"
+          className={styles.backImagePhone}
         />
       </div>
     </div></>
