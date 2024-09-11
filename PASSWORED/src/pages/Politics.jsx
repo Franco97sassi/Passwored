ebfacfb5ae29f28c561e8a551f37077a256fb763
franco sassi <franco.sassi97@gmail.com>
2024-09-11 13:47:32 -0300

    polity changes

--- PASSWORED/src/pages/Politics.jsx
+++ PASSWORED/src/pages/Politics.jsx
@@ -22,7 +22,7 @@ const Politics = () => {
       <p className={styles.politicsp}>
         Para los propósitos de esta Política de Privacidad:
       </p>
-      <ul className={styles.politicsul}>
+      <ol className={styles.politicsul}>
         <li><strong>Cuenta</strong> significa una cuenta única creada para que accedas a nuestro Servicio o partes de nuestro Servicio.</li>
         <li><strong>Afiliado</strong> significa una entidad que controla, es controlada por o está bajo control común con una parte, donde “control” significa la propiedad del 50% o más de las acciones, participación accionaria u otros valores con derecho a voto para la elección de directores u otra autoridad de gestión.</li>
         <li><strong>Aplicación</strong> se refiere a PASSWORED, el programa de software proporcionado por la Compañía.</li>
@@ -34,7 +34,7 @@ const Politics = () => {
         <li><strong>Proveedor de Servicios</strong> significa cualquier persona natural o jurídica que procesa los datos en nombre de la Compañía. Se refiere a empresas o individuos terceros empleados por la Compañía para facilitar el Servicio, para proporcionar el Servicio en nombre de la Compañía, para realizar servicios relacionados con el Servicio o para ayudar a la Compañía a analizar cómo se utiliza el Servicio.</li>
         <li><strong>Datos de Uso</strong> se refiere a los datos recopilados automáticamente, ya sea generados por el uso del Servicio o de la infraestructura del Servicio en sí (por ejemplo, la duración de una visita a la página).</li>
         <li><strong>Tú</strong> significa el individuo que accede o usa el Servicio, o la empresa u otra entidad legal en nombre de la cual dicho individuo está accediendo o usando el Servicio, según corresponda.</li>
-      </ul>
+      </ol>
 
       <h2  className={styles.politicsh2}>3. Recopilación y Uso de tus Datos Personales</h2>
 
@@ -44,11 +44,11 @@ const Politics = () => {
       <p className={styles.politicsp}>
         Mientras usas nuestro Servicio, podemos pedirte que nos proporciones cierta información de identificación personal que puede ser utilizada para contactarte o identificarte. La información de identificación personal puede incluir, pero no se limita a:
       </p>
-      <ul className={styles.politicsul}>
+      <ol className={styles.politicsul}>
         <li>Dirección de correo electrónico</li>
         <li>Nombre y apellido</li>
         <li>Datos de Uso</li>
-      </ul>
+      </ol>
 
       <h4  className={styles.politicsh4}>Datos de Uso</h4>
       <p className={styles.politicsp}>
@@ -68,31 +68,31 @@ const Politics = () => {
       <p className={styles.politicsp}>
         La Compañía puede usar los Datos Personales para los siguientes propósitos:
       </p>
-      <ul  className={styles.politicsul}>
+      <ol  className={styles.politicsul}>
         <li>Para proporcionar y mantener nuestro Servicio, incluyendo para monitorear el uso de nuestro Servicio.</li>
         <li>Para gestionar tu Cuenta: para gestionar tu registro como usuario del Servicio. Los Datos Personales que proporcionas pueden darte acceso a diferentes funcionalidades del Servicio que están disponibles para ti como usuario registrado.</li>
         <li>Para personalizar el contenido.</li>
         <li>Para formular estadísticas.</li>
         <li>Para mejorar la app.</li>
-      </ul>
+      </ol>
 
       <h2  className={styles.politicsh2}>5. Seguridad de los Datos</h2>
       <p className={styles.politicsp}>
         Implementamos las siguientes medidas de seguridad para proteger los datos de los usuarios:
       </p>
-      <ul  className={styles.politicsul}>
+      <ol  className={styles.politicsul}>
         <li><strong>Cifrado de Datos:</strong> Utilizamos cifrado SSL/TLS para proteger los datos durante la transmisión.</li>
         <li><strong>Almacenamiento Seguro:</strong> Aseguramos que los datos se almacenen en servidores seguros con acceso restringido.</li>
-      </ul>
+      </ol>
 
       <h2  className={styles.politicsh2}>6. Protección de la Privacidad de los Niños</h2>
       <p className={styles.politicsp}>
         Para proteger la privacidad de los niños, tomamos las siguientes medidas:
       </p>
-      <ul  className={styles.politicsul}>
+      <ol  className={styles.politicsul}>
         <li><strong>Consentimiento Parental:</strong> Obtenemos el consentimiento verificable de los padres o tutores antes de recopilar cualquier dato personal de los niños.</li>
         <li><strong>Limitación de Datos:</strong> Recopilamos solo la información necesaria y evitamos recopilar datos sensibles.</li>
-      </ul>
+      </ol>
 
       <h2  className={styles.politicsh2}>7. Derechos de los Usuarios</h2>
       <p className={styles.politicsp}>
